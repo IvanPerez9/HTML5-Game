582ce4b1ed7613671dc96b2e3f31cadd3a550568
IVÁN_P\Iván <ivan120695@gmail.com>
2018-01-29 22:36:03 +0100

    gif de cargar

--- js/game.js
+++ js/game.js
@@ -1,28 +1,28 @@
-$(window).load(function(){
-               game.init();
+$(window).load(function () {
+    game.init();
 });
 
 var game = {
-    
+
     // Comienzo inicializando los objetos, precargando los assets y mostrando la pantalla de inicio
-    init: function(){
+    init: function () {
         //Inicializa objetos
         levels.init();
         loader.init();
         //mouse.init();
-        
+
         //Oculta todas la capas del juego y muestra la pantalla de inicio
         $('.gamelayer').hide();
         $('#gamestartscreen').show();
-        
+
         //Obtener el controlador para el canvas y el contexto del juego
         game.canvas = $('#gamecanvas')[0];
         game.context = game.canvas.getContext('2d');
     },
 
-    showLevelScreen:function(){
-		$('.gamelayer').hide(); // Oculta todas las capas del juego
-		$('#levelselectscreen').show('slow'); // Muestra la de level
+    showLevelScreen: function () {
+        $('.gamelayer').hide(); // Oculta todas las capas del juego
+        $('#levelselectscreen').show('slow'); // Muestra la de level
     },
 
 }
@@ -31,82 +31,81 @@ var game = {
 
 var levels = {
     //Nivel de datos. Tiene un array con info acerca de cada nivel
-    data:[
+    data: [
         { // Primer nivel
             foreground: 'desert-foreground',
             background: 'clouds-background',
-            entities:[]
+            entities: []
         },
         { // Segundo nivel
             foreground: 'desert-foreground',
             background: 'clouds-background',
-            entities:[]
+            entities: []
         }
     ],
-    
+
     // Inicializa la pantalla de seleccion de nivel
-    init:function(){
+    init: function () {
         var html = "";
-        for(var i=0; i<levels.data.length; i++){
+        for (var i = 0; i < levels.data.length; i++) {
             var level = levels.data[i];
-            html += ' <input type="button"  value="'+(i+1)+'">';
+            html += ' <input type="button"  value="' + (i + 1) + '">';
         };
         $('#levelselectscreen').html(html);
         //Poner el botton para cargar el nivel
-        $('#levelselectscreen input').click(function(){
-            levels.load(this.value-1);
+        $('#levelselectscreen input').click(function () {
+            levels.load(this.value - 1);
             $('#levelselectscreen').hide();
         });
     },
-    
-    //carga todos los datos e imagenes para un nivel especifico
-    load:function(number){
-        //declarar un nuevo objeto de nivel actual
-        game.currentLevel = {number:number ,hero:[]};
+
+    //Carga todos los datos e imagenes para un nivel especifico
+    load: function (number) {
+        //Declarar un nuevo objeto de nivel actual
+        game.currentLevel = {number:number, hero:[]};
         game.score = 0;
-        $('#score').html('Score: '+game.score);
+        $('#score').html('Score: ' +game.score);
         var level = levels.data[number];
 
-        //Cargar el fondo el primer plano y las imagenes de la honda
-        game.currentLevel.backgroundImage = loader.loadImage("../images/backgrounds/"+levle.background+".png");
-        game.currentLevel.foregroundImage = loader.loadImage("../images/backgrounds/"+levle.foreground+".png"); 
+        //Cargar el fondo, el primer plano y las imagenes de la honda
+        game.currentLevel.backgroundImage = loader.loadImage("../images/backgrounds/" + level.background + ".png");
+        game.currentLevel.foregroundImage = loader.loadImage("../images/backgrounds/" + level.foreground + ".png");
         game.slingshotImage = loader.loadImage("../images/slingshot.png");
         game.slingshotFrontImage = loader.loadImage("../images/slingshot-front.png");
 
-        //Llamar a game start() cuando los assets se hayan cargado
-        if(loader.loaded){
+        //Llamar a game.start() cuando los assets se hayan cargado
+        if (loader.loaded) {
             game.start()
-        }else{
+        } else {
             loader.onload = game.start;
         }
-    }
-    
+    },
 }
 
 var loader = {
+    loaded: true,
+    loadedCount: 0, //Assets que han sido cargados antes
+    totalCount: 0, //Numero total de assets que es necesario cargar
 
-    loader:true,
-    loaderCount:0, // Cargados antes
-    totalCount:0, // Numero total necesarios a cargar
-
-    init:function(){
-        //Comprueba el soporte para el sonido
-        var mp3Support,oggSupport;
+    init: function () {
+        //Comprueba el soporte para sonido
+        var mp3Support, oggSupport;
         var audio = document.createElement('audio');
-        if(audio.canPlayType){
+        if (audio.canPlayType) {
+            //Actualmente canPlayType() devuelve: "","maybe","probaly"
             mp3Support = "" != audio.canPlayType('audio/mpeg');
             oggSupport = "" != audio.canPlayType('audio/ogg; codecs="vorbis"');
         } else {
-            // Si no es correcto
+            //La etiqueta de audio no es soportada
             mp3Support = false;
             oggSupport = false;
         }
-
+        //Comprueba para ogg o mp3 y si no finalmente fija soundFileExtn como undefined
         // Comprobar para ogg y mp3 si fija soundFileExt como undefined
-        loader.soundFileExt = oggSupport?".ogg":mp3Support?".mp3":undefined; // Operador terciario como en Java
+        loader.soundFileExtn = oggSupport ? ".ogg" : mp3Support ? ".mp3" : undefined;
     },
 
-    loadImage:function(url){
+    loadImage: function (url) {
         this.totalCount++;
         this.loaded = false;
         $('#loadingscreen').show();
@@ -115,32 +114,28 @@ var loader = {
         image.onload = loader.itemLoaded;
         return image;
     },
-
-    soundFileExt:".ogg",
-    loadSound:function(url){
+    soundFileExtn: ".ogg",
+    loadSound: function (url) {
         this.totalCount++;
         this.loaded = false;
         $('#loadingscreen').show();
         var audio = new Audio();
-        audio.src = url+loader.soundFileExt;
-        audio.addEventListener("canplaythrough", loader.itemLoaded, false);
+        audio.src = url + loader.soundFileExtn;
+        audio.addEventListener("canplaythrough", loader.loadImage, false);
         return audio;
     },
-
-    itemLoaded:function(){
+    itemLoaded: function () {
         loader.loadedCount++;
-        $('#loadingmessage').html('Loaded ' +loader.loadedCount+ ' of ' +loader.totalCount);
-        if(loader.loadedCount === loader.totalCount){
-            // Se carga completamente
+        $("#loadingmessage").html('Loaded' + loader.loadedCount + ' of ' + loader.totalCount);
+        if (loader.loadedCount === loader.totalCount) {
+            //El loader ha cargado completamente...
             loader.loaded = true;
-            // Ocultar la pantalla de carga
+            // Oculta la pantalla de carga
             $('#loadingscreen').hide();
-            // Y llama al metodo loade si exite
-            if(loader.onload){
+            if (loader.onload) {
                 loader.onload();
                 loader.onload = undefined;
             }
         }
     }
-
 }
