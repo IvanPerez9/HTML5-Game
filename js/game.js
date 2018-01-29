01c6d3bc2562123eca7ab812638855602b74c1f7
IVÁN_P\Iván <ivan120695@gmail.com>
2018-01-29 22:17:37 +0100

    Levels

--- js/game.js
+++ js/game.js
@@ -2,6 +2,31 @@ $(window).load(function(){
                game.init();
 });
 
+var game = {
+    
+    // Comienzo inicializando los objetos, precargando los assets y mostrando la pantalla de inicio
+    init: function(){
+        //Inicializa objetos
+        levels.init();
+        loader.init();
+        //mouse.init();
+        
+        //Oculta todas la capas del juego y muestra la pantalla de inicio
+        $('.gamelayer').hide();
+        $('#gamestartscreen').show();
+        
+        //Obtener el controlador para el canvas y el contexto del juego
+        game.canvas = $('#gamecanvas')[0];
+        game.context = game.canvas.getContext('2d');
+    },
+
+    showLevelScreen:function(){
+		$('.gamelayer').hide(); // Oculta todas las capas del juego
+		$('#levelselectscreen').show('slow'); // Muestra la de level
+    },
+
+}
+
 //Objeto levels
 
 var levels = {
@@ -24,14 +49,16 @@ var levels = {
         var html = "";
         for(var i=0; i<levels.data.length; i++){
             var level = levels.data[i];
-            html += '<input type="button"  value="'+(i+1)+'">';
+            html += ' <input type="button"  value="'+(i+1)+'">';
         };
         $('#levelselectscreen').html(html);
+        //Poner el botton para cargar el nivel
         $('#levelselectscreen input').click(function(){
             levels.load(this.value-1);
             $('#levelselectscreen').hide();
         });
     },
+    
     //carga todos los datos e imagenes para un nivel especifico
     load:function(number){
         //declarar un nuevo objeto de nivel actual
@@ -41,10 +68,10 @@ var levels = {
         var level = levels.data[number];
 
         //Cargar el fondo el primer plano y las imagenes de la honda
-        game.currentLevel.backgroundImage = loader.loadImage("images/backgrounds/"+levle.background+".png");
-        game.currentLevel.foregroundImage = loader.loadImage("images/backgrounds/"+levle.foreground+".png"); 
-        game.slingshotImage = loader.loadImage("images/slingshot.png");
-        game.slingshotFrontImage = loader.loadImage("images/slingshot-front.png");
+        game.currentLevel.backgroundImage = loader.loadImage("../images/backgrounds/"+levle.background+".png");
+        game.currentLevel.foregroundImage = loader.loadImage("../images/backgrounds/"+levle.foreground+".png"); 
+        game.slingshotImage = loader.loadImage("../images/slingshot.png");
+        game.slingshotFrontImage = loader.loadImage("../images/slingshot-front.png");
 
         //Llamar a game start() cuando los assets se hayan cargado
         if(loader.loaded){
@@ -115,32 +142,5 @@ var loader = {
             }
         }
     }
-
-}
-
-// Mirar cambios en el .js 29/01 
-
-var game = {
-    
-    // Comienzo inicializando los objetos, precargando los assets y mostrando la pantalla de inicio
-    init: function(){
-        //Inicializa objetos
-        levels.init();
-        loader.init();
-        mouse.init();
-        
-        //Oculta todas la capas del juego y muestra la pantalla de inicio
-        $('.gamelayer').hide();
-        $('#gamestartscreen').show();
-        
-        //Obtener el controlador para el canvas y el contexto del juego
-        game.canvas = $('#gamecanvas')[0];
-        game.context = game.canvas.getContext('2d');
-    },
-
-    showLevelScreen:function(){
-		$('.gamelayer').hide(); // Oculta todas las capas del juego
-		$('#levelselectscreen').show('slow'); // Muestra la de level
-    },
 
 }
