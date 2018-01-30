4b95530377dad87872b20089dd116c2b09524e48
IVÁN_P\Iván <ivan120695@gmail.com>
2018-01-30 17:27:44 +0100

    Cambio pre-clase
    30/01 cambios antes de la clase, deberi ir los niveles.

--- js/game.js
+++ js/game.js
@@ -1,3 +1,27 @@
+// Preparar requestAnimationFrame y cancelAnimationFrame para su uso en el codigo del juego
+(function () {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+            window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function (callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+                timeToCall); lastTime = currTime + timeToCall; return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+}());
+
+// Inicio
 $(window).load(function () {
     game.init();
 });
@@ -9,7 +33,7 @@ var game = {
         //Inicializa objetos
         levels.init();
         loader.init();
-        //mouse.init();
+        mouse.init();
 
         //Oculta todas la capas del juego y muestra la pantalla de inicio
         $('.gamelayer').hide();
@@ -24,6 +48,88 @@ var game = {
         $('.gamelayer').hide(); // Oculta todas las capas del juego
         $('#levelselectscreen').show('slow'); // Muestra la de level
     },
+
+    // Modo Game
+    mode: "intro",
+    // Coordenadas de la honda , X e Y 
+    slingshotX: 140,
+    slingshotY: 280,
+    start: function () {
+        $('.gamelayer').hide();
+        // Mostrar el canvas del juego y la puntacion 
+        $('#gamecanvas').show();
+        $('#scorescreen').show();
+
+        //game.startBackgroundMusic();
+
+        game.mode = "intro"; // Almacena el estado del juego actual
+        game.offsetLeft = 0;
+        game.ended = false;
+        game.animationFrame = window.requestAnimationFrame(game.animate, game.canvas);
+        // Fija el intervalo de animacion del juego 
+    },
+
+    // Velocidad maxima de fotograma
+	maxSpeed:3,
+	// minimo y maximo de offset
+	minOffset:0,
+	maxOffset:300,
+	// Desplazamiento de panorámica actual
+	offsetLeft:0,
+	// La puntuacion del juego.
+	score:0,
+
+	//Desplegar la pantalla para centrarse en newCenter
+	panTo:function(newCenter){
+		if (Math.abs(newCenter-game.offsetLeft-game.canvas.width/4)>0
+			&& game.offsetLeft <= game.maxOffset && game.offsetLeft >= game.minOffset){
+
+			var deltaX = Math.round((newCenter-game.offsetLeft-game.canvas.width/4)/2);
+			if (deltaX && Math.abs(deltaX)>game.maxSpeed){
+				deltaX = game.maxSpeed*Math.abs(deltaX)/(deltaX);
+			}
+			game.offsetLeft += deltaX;
+		} else {
+
+			return true;
+		}
+		if (game.offsetLeft <game.minOffset){
+			game.offsetLeft = game.minOffset;
+			return true;
+		} else if (game.offsetLeft > game.maxOffset){
+			game.offsetLeft = game.maxOffset;
+			return true;
+		}
+		return false;
+    },
+    
+    // ESTO LO CAMBIO !!!!! ??? ?
+    
+    handlePanning: function () {
+        game.offsetLeft++; // Marca de posicion temporal, mantiene la panoramica a la derecha
+    }, // Errata en las diapos 
+
+    animate: function () {
+        //Anima el fondo
+        game.handlePanning();
+
+        //Anima los personajes
+
+        //Dibuja el fondo con un desplazamiento 
+        game.context.drawImage(game.currentLevel.backgroundImage, game.offsetLeft / 4, 0, 640, 480, 0, 0, 640, 480);
+        game.context.drawImage(game.currentLevel.foregroundImage, game.offsetLeft, 0, 640, 480, 0, 0, 640, 480);
+        // Se mueven a diferentes velocidades.
+
+        //Dibuja la honda 
+        game.context.drawImage(game.slingshotImage, game.slingshotX - game.offsetLeft, game.slingshotY);
+        game.context.drawImage(game.slingshotFrontImage, game.slingshotX - game.offsetLeft, game.slingshotY);
+
+        if (!game.ended) {
+            game.animationFrame = window.requestAnimationFrame(game.animate, game.canvas);
+        }
+    }
+
+
 
 }
 
@@ -62,9 +168,9 @@ var levels = {
     //Carga todos los datos e imagenes para un nivel especifico
     load: function (number) {
         //Declarar un nuevo objeto de nivel actual
-        game.currentLevel = {number:number, hero:[]};
+        game.currentLevel = { number: number, hero: [] };
         game.score = 0;
-        $('#score').html('Score: ' +game.score);
+        $('#score').html('Score: ' + game.score);
         var level = levels.data[number];
 
         //Cargar el fondo, el primer plano y las imagenes de la honda
@@ -138,4 +244,40 @@ var loader = {
             }
         }
     }
+}
+
+var mouse = {
+	x:0,
+	y:0,
+    down:false,
+    // Fija los elementos para cuando se mueve, o presiones.
+	init:function(){
+		$('#gamecanvas').mousemove(mouse.mousemovehandler);
+		$('#gamecanvas').mousedown(mouse.mousedownhandler);
+		$('#gamecanvas').mouseup(mouse.mouseuphandler);
+		$('#gamecanvas').mouseout(mouse.mouseuphandler);
+    },
+    // Calcula las coordenadas X e Y del raton respecto a la esquina superior izquierda
+	mousemovehandler:function(ev){
+		var offset = $('#gamecanvas').offset();
+
+		mouse.x = ev.pageX - offset.left;
+		mouse.y = ev.pageY - offset.top;
+
+		if (mouse.down) {
+			mouse.dragging = true;
+		}
+    },
+    // Almacena la ubicacion de donde está el raton.
+	mousedownhandler:function(ev){
+		mouse.down = true;
+		mouse.downX = mouse.x;
+		mouse.downY = mouse.y;
+		ev.originalEvent.preventDefault();
+
+	},
+	mouseuphandler:function(ev){
+		mouse.down = false;
+		mouse.dragging = false;
+	}
 }
