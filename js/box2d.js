6187a4276b2d79ba45927ac493863127b4b2507a
IVÁN_P\Iván <ivan120695@gmail.com>
2018-02-08 22:29:40 +0100

    Last test

--- js/box2d.js
+++ js/box2d.js
@@ -26,6 +26,8 @@ function init(){
     createSimplePolyonBody();
     //Crear un cuerpo combinado de dos formas
     createComplexBody();
+    //Unir dos cuerpos mediante un joint
+    createRevoluteJoint();
 
     setupDebugDraw();
     animate(); // 105 
@@ -185,4 +187,54 @@ function createComplexBody(){
     body.CreateFixture(fixtureDef);
 }
 
-// 126 
+function createRevoluteJoint(){
+    //Definir el primer cuerpo
+    var bodyDef1 = new b2BodyDef;
+    bodyDef1.type = b2Body.b2_dynamicBody;
+    bodyDef1.position.x = 480/scale;
+    bodyDef1.position.y = 60/scale;
+    var body1 = world.CreateBody(bodyDef1);
+
+    //Crear el primer accesorio y añadir la forma rectangular al cuerpo
+    var fixtureDef1 = new b2FixtureDef;
+    fixtureDef1.density = 1.0;
+    fixtureDef1.friction = 0.5;
+    fixtureDef1.restitution = 0.5;
+    fixtureDef1.shape = new b2PolygonShape;
+    fixtureDef1.shape.SetAsBox(50/scale, 10/scale);
+
+    body1.CreateFixture(fixtureDef1);
+
+    //Definir el segundo cuerpo
+    var bodyDef2 = new b2BodyDef;
+    bodyDef2.type = b2Body.b2_dynamicBody;
+    bodyDef2.position.x = 470/scale;
+    bodyDef2.position.y = 50/scale;
+    var body2 = world.CreateBody(bodyDef2);
+
+    //Crear el segundo accesorio y añadir la forma rectangular al cuerpo
+    var fixtureDef2 = new b2FixtureDef;
+    fixtureDef2.density = 1.0;
+    fixtureDef2.friction = 0.5;
+    fixtureDef2.restitution = 0.5;
+    fixtureDef2.shape = new b2PolygonShape;
+    var points = [
+        new b2Vec2(0,0),
+        new b2Vec2(40/scale, 50/scale),
+        new b2Vec2(50/scale, 100/scale),
+        new b2Vec2(-50/scale , 100/scale),
+        new b2Vec2(-40/scale , 50/scale),
+    ];
+
+    fixtureDef2.shape.SetAsArray(points,points.length);
+    body2.CreateFixture(fixtureDef2);
+
+    //Crear una articulacion entre el body1 y el body2
+    var jointDef = new b2RevoluteJointDef;
+    var jointCenter = new b2Vec2(470/scale, 50/scale);
+
+    jointDef.Initialize(body1,body2,jointCenter);
+    world.CreateJoint(jointDef);
+}
+
+//132
